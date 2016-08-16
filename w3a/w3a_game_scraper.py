170423c99087c96b6cd5eb8cece591ce79211248
Ben Messerly <bamesserly@gmail.com>
2016-08-15 21:22:47 -0500

    Some more improvements
    Now saves the games to the list of dicts of new games.
    
    Still lots of conditions to deal with.
    
    The "issue" from the last commit was just me forgetting to remove 2v2.

--- w3a/w3a_game_scraper.py
+++ w3a/w3a_game_scraper.py
@@ -6,8 +6,12 @@ import datetime
 import time
 #new_games_temp = open('new_games.txt', 'w+')
 
+date_of_last_scan = '19-06-2016 00:00'
+game_fields = ['date_time', 'winning_player','player1_name','player2_name']
 part_list_of_games = []
 progress_notifier = []
+
+list_of_new_games = []
 
 for tag in dict_of_elos:
   print tag
@@ -30,40 +34,78 @@ for tag in dict_of_elos:
           #can happen with first or last page that doesn't have full 20 games
           pass
 
-        n_new_games_scanned += 1
-        if( datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M") < datetime.datetime.strptime('19-06-2016 00:00', "%d-%m-%Y %H:%M")):
-          print "  most recent game " + str(datetime.datetime.strptime(most_recent_game_time, '%Y-%m-%d %H:%M'))
-          print "  current game     " + str(datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M"))
+        # Check if solo
+        if game_results[1] != "1on1":
+          continue
 
-        #Check time condition
+        # Check if we have caught up to the most recent game
         if(datetime.datetime.strptime(most_recent_game_time, '%Y-%m-%d %H:%M') >= datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M")):
-          if( datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M") < datetime.datetime.strptime('19-06-2016 00:00', "%d-%m-%Y %H:%M")):
-            print "  we've read all the games up until the last one on record"
-            print "    most recent game " + str(datetime.datetime.strptime(most_recent_game_time, '%Y-%m-%d %H:%M'))
-            print "    current game     " + str(datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M"))
-          n_new_games_scanned -= 1
-          #print "    new games retreived = " + str(n_new_games_scanned)
+          if n_new_games_scanned > 0:
+            print "    most recent game " + str(most_recent_game_time)
+            print "    new games retreived = " + str(n_new_games_scanned)
           keep_searching_for_games = False
           break #for x in range (3, len(item))
+
+        # Check if this game is before we last scanned (so we missed it)
+        if( datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M") < datetime.datetime.strptime(date_of_last_scan, "%d-%m-%Y %H:%M")): 
+          print "  WARNING: GAME(S) AFTER MOST RECENT ON RECORD BUT BEFORE 6-19-2016"
+          print "    current game     " + str(datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M"))
+
+        # Get the game info and save it
+        game_dict = {}
+        game_dict.fromkeys(game_fields)
+        game_dict['date_time'] = game_results[4].encode("utf-8")
+        if (tag == game_results[2]) is not (tag == game_results[3]):
+          game_dict['player1_name'] = tag
+          game_dict['player2_name'] = game_results[3] if (game_results[2] == tag) else (game_results[2])
+          game_dict['winning_player'] = tag if (game_results[0] == 'Win') else (game_dict['player2_name'])
+        else:
+          print "WARNING: CURRENT PLAYER IS NEITHER P1 NOR P2"
+          print "tag " + tag
+          print "player1 " + game_dict['player1_name']
+          print "player2 " + game_dict['player2_name']
+
+        n_new_games_scanned += 1
+        list_of_new_games.append(game_dict)
+      # Got to the end of the page, going to the next one
       else:
-        print "I finished the x in range loop, I'm moving on to page " + str(page+1)
+        print "  End page. Going to next page: " + str(page+1)
         page +=1
         continue
-      #break #for item in page_data
+
 '''
+          if game_results[0] == "Win":
+            game_info['winning_player'] = tag
+            game_info['player2_name'] = game_results[2] if (game_results[1] == tag) else game_results[1]
+          else:
+            game_info['winning_player'] = game_results[2]
+            game_info['player2_name'] = game_results[2] if (game_results 
+          game_info['player1_name'] = tag
+          game_info['player2_name'] = game_results[2] if (game_results[1] == game_info['player1_name']) else game_results[1]
+          game_info['winning_player'] = tag if game_results[0] == tag else
+        else:
+
+
+      #break
+
+  {'date_time': '08-09-2015 13:50', 'player1_name': 'zzz', 'player2_name': 'H1mmla', 'winning_player': 'zzz'}
   url = "http://tft.w3arena.net/profile/" + tag + "/?p=0"
   #url = "http://tft.w3arena.net/profile/" + "FaceControl" + "/?p=0"
   #url = "http://tft.w3arena.net/profile/" + "HuntressShaped" + "/?p=0"
-    #print url
-    #print soup.prettify()
-    #print page_data
+  #print url
+  #print soup.prettify()
+  #print page_data
+  #if( datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M") < datetime.datetime.strptime('19-06-2016 00:00', "%d-%m-%Y %H:%M")):
+    #print "  we've read all the games up until the last one on record"
+    #print "    most recent game " + str(datetime.datetime.strptime(most_recent_game_time, '%Y-%m-%d %H:%M'))
+    #print "    current game     " + str(datetime.datetime.strptime(game_results[4], "%d-%m-%Y %H:%M"))
 
-      if (datetime.datetime.strptime(game_date_time, '%Y-%m-%d %H:%M') <
+  if (datetime.datetime.strptime(game_date_time, '%Y-%m-%d %H:%M') <
 
 
-      game_date_time = time.strftime("%Y-%m-%d %H:%M", time.strptime(game_date_time,"%d-%m-%Y %H:%M")) 
+  game_date_time = time.strftime("%Y-%m-%d %H:%M", time.strptime(game_date_time,"%d-%m-%Y %H:%M")) 
 
-27-07-2015 08:55
+  27-07-2015 08:55
 
     #game_date_time = time.strftime("%Y-%m-%d %H:%M", time.strptime(game_date_time,"%d-%m-%Y %H:%M"))
 
