bf83b22c23d0e0cf4146623bbc4584e102520833
Ben Messerly <bamesserly@gmail.com>
2018-05-25 16:33:13 -0500

    DBox API v2
    Also, remove my personal DBox tokens from repo.

--- w3a/UploadToDropbox.py
+++ w3a/UploadToDropbox.py
@@ -3,18 +3,19 @@
 
 from transferdata import TransferData
 from Utils        import top
-from time import sleep 
+from time         import sleep 
+from dbox_token   import token
 
 def UploadToDropbox(spreadsheet_file):
   print "Entering UploadToDropbox()"
-  access_token = 'BJfeECR3F-AAAAAAAAAACfdM5D9QFy0fITmIYyAn_hC7qTWfEBH-rJ_1RrTN5gaL'	# get your access token from Dropbox Developers
+  access_token = token # get your access token from Dropbox Developers
   transferData = TransferData(access_token)
 
   file_from = top + spreadsheet_file
-  file_to   = spreadsheet_file	# The full path to upload the file to, including the file name
-  
-  # API v1
-  transferData.upload_file_v1(file_from=file_from, file_to=file_to)
+  file_to   = "/" + spreadsheet_file	# The full path to upload the file to, including the file name
+
+  # API v2
+  transferData.upload_file(file_from=file_from, file_to=file_to)
 
   print "Exiting UploadToDropbox()"
 
